9cd9e636c2f38ae918c7198611a4021d97ed5de1
sings59 <sings59@mcmaster.ca>
2021-10-07 22:35:02 -0400

    added button and header components

---
+++ src/Components/Button.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+const Button = ({color,text}) => {
+
+    const onClick = (e) =>{
+        console.log(e)
+    }
+    return (
+        <button onClick = {onClick} style= {{backgroundColor : color}} 
+        className = 'btn'>
+            {text}
+        </button>
+    )
+}
+
+Button.defaultProps ={
+    color: 'grey'
+}
+
+Button.propTypes ={
+    text: PropTypes.string,
+    color: PropTypes.string,
+}
+export default Button
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import Button  from'./Button'
+const Header = () => {
+
+    return(
+        <header>
+            <h1>Task Tracker</h1>
+            <Button color = 'red' text = 'Hello' />
+            <Button text = 'Hello world'/>
+        </header>
+    )
+}
+
+export default Header
